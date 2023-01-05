3a95ae1e468a6254d58e1adf901505e09272eb73
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-01-05 14:24:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ps705112/URL-Shortener-V2
+  git clone https://github.com/CyniteOfficial/Channel-Mdisk-Converter
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /Channel-Mdisk-Converter
 fi
-cd /URL-Shortener-V2
+cd /Channel-Mdisk-Converter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
