6de42a059128dea65bbdaeb1e1652e26589dd1c8
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-01-05 15:45:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Channel-Mdisk-Converter.git /Channel-Mdisk-Converter
+  git clone https://github.com/CyniteOfficial/Channel-Mdisk-Converter
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  echo "Cloning Custom Repo from $SOURCE_CODE "
   git clone $SOURCE_CODE /Channel-Mdisk-Converter
 fi
 cd /Channel-Mdisk-Converter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 main.py
